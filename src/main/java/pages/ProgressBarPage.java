8a407092df4c54d63cc3028c16f5db5afa238f56
Emilsrie <67155950+Emilsrie@users.noreply.github.com>
2023-08-24 18:43:01 +0300

    update code

--- src/main/java/pages/ProgressBarPage.java
+++ src/main/java/pages/ProgressBarPage.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import pages.objects.Button;
 import pages.objects.Link;
+import pages.objects.Text;
 
 public class ProgressBarPage {
 
     public Link mainLogo = new Link("Main logo", "css=a > img");
-
     public Button randomProgressBarButton = new Button("Random progress bar button", "xpath=/html/body/div/div[1]/div[2]/div/div/div[2]/div/ul/li[2]");
 
     public ProgressBarPage(WebDriver driver) {
@@ -22,4 +22,5 @@ public class ProgressBarPage {
     public void clickRandomProgressBarButton() {
         this.randomProgressBarButton.click();
     }
+
 }
